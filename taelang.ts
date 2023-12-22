6f7279530c1a9f012c7feef265135482ef152d36
Victor Taelin <victor.taelin@gmail.com>
2023-12-22 02:06:59 -0300

    explicit instantiator for self

--- taelang.ts
+++ taelang.ts
@@ -20,22 +20,24 @@ export type Term =
   | { tag: "Lam"; bod: (x: Term) => Term } // λx <term>
   | { tag: "App"; fun: Term; arg: Term } // (<term> <term>)
   | { tag: "Slf"; bod: (x: Term) => Term } // $<x> <term>
+  | { tag: "Ins"; val: Term } // ~<term>
   | { tag: "Ann"; chk: boolean; val: Term; typ: Term } // {<term> : <term>}
   | { tag: "Let"; val: Term; bod: (x: Term) => Term } // let x = val; body
   | { tag: "Def"; val: Term; bod: (x: Term) => Term } // def x = val; body
   | { tag: "Ref"; nam: string } // @term
-  | { tag: "Hol"; nam: string, ctx: Context } // ? hole
+  | { tag: "Hol"; nam: string, ctx: Context, got: List<Term> } // ? hole
 export const Var = (val: number): Term => ({ tag: "Var", val });
 export const Set = (): Term => ({ tag: "Set" });
 export const All = (inp: Term, bod: (x: Term) => Term): Term  => ({ tag: "All", inp, bod });
 export const Lam = (bod: (x: Term) => Term): Term => ({ tag: "Lam", bod });
 export const App = (fun: Term, arg: Term): Term => ({ tag: "App", fun, arg });
 export const Slf = (bod: (x: Term) => Term): Term => ({ tag: "Slf", bod });
+export const Ins = (val: Term): Term => ({ tag: "Ins", val });
 export const Ann = (chk: boolean, val: Term, typ: Term): Term => ({ tag: "Ann", chk, val, typ });
 export const Let = (val: Term, bod: (x: Term) => Term): Term => ({ tag: "Let", val, bod });
 export const Def = (val: Term, bod: (x: Term) => Term): Term => ({ tag: "Def", val, bod });
 export const Ref = (nam: string): Term => ({ tag: "Ref", nam });
-export const Hol = (nam: string, ctx: Context): Term => ({ tag: "Hol", nam, ctx });
+export const Hol = (nam: string, ctx: Context, got: List<Term>): Term => ({ tag: "Hol", nam, ctx, got });
 
 // A type context
 export type Context = List<[string, Term]>;
@@ -47,6 +49,8 @@ export type Load = (key: string) => ({typ: Term, val: Term}) | null;
 // Checker
 // -------
 
+var FRESH = 0;
+
 // Reduces to weak normal form
 export function reduce(load: Load, term: Term, dep: number): Term {
   if (term.tag === "App") {
@@ -54,9 +58,15 @@ export function reduce(load: Load, term: Term, dep: number): Term {
     if (fun.tag === "Lam") {
       return reduce(load, fun.bod(term.arg), dep);
     }
+    if (fun.tag === "Hol") {
+      return reduce(load, Hol(fun.nam, fun.ctx, Cons(term.arg, fun.got)), dep);
+    }
     return term;
   }
   if (term.tag === "Ann") {
+    return reduce(load, term.val, dep);
+  }
+  if (term.tag === "Ins") {
     return reduce(load, term.val, dep);
   }
   if (term.tag === "Let") {
@@ -91,6 +101,8 @@ export function equal(load: Load, a: Term, b: Term, dep: number): boolean {
     eq = equal(load, a.fun, b.fun, dep) && equal(load, a.arg, b.arg, dep);
   } else if (a.tag === "Slf" && b.tag === "Slf") {
     eq = equal(load, a.bod(Var(dep)), b.bod(Var(dep)), dep + 1);
+  } else if (a.tag === "Ins" && b.tag === "Ins") {
+    eq = equal(load, a.val, b.val, dep);
   } else if (a.tag === "Ref" && b.tag === "Ref" && a.nam === b.nam) {
     eq = true;
   } else if (a.tag === "Hol" && b.tag === "Hol" && a.nam === b.nam) {
@@ -100,10 +112,10 @@ export function equal(load: Load, a: Term, b: Term, dep: number): boolean {
   } else if (b.tag === "Lam") {
     eq = equal(load, Lam(x => App(a, x)), b, dep);
   } else if (a.tag === "Hol") {
-    console.log("NOTE: ?" + a.nam + " = " + show_term(b, dep));
+    console.log("NOTE: " + show_term(a, dep) + " = " + show_term(b, dep));
     eq = true;
   } else if (b.tag === "Hol") {
-    console.log("NOTE: ?" + b.nam + " = " + show_term(a, dep));
+    console.log("NOTE: " + show_term(b, dep) + " = " + show_term(a, dep));
     eq = true;
   }
   if (!eq) {
@@ -146,7 +158,7 @@ export function infer(load: Load, val: Term, dep: number = 0): Term {
       return loaded !== null ? loaded.typ : Ref(val.nam);
     }
     case "Hol": {
-      throw "TODO";
+      return Hol(val.nam+"_T", val.ctx, val.got);
     }
     case "Set": {
       return Set();
@@ -157,15 +169,34 @@ export function infer(load: Load, val: Term, dep: number = 0): Term {
       return Set();
     }
     case "Lam": {
-      throw "NonFunLam";
+      return All(
+        Hol(num_to_str(FRESH++), Nil(), Nil()), x =>
+        Hol(num_to_str(FRESH++), Cons([num_to_str(dep),x], Nil()), Nil()));
     }
     case "App": {
       var fun_ty = reduce(load, infer(load, val.fun, dep), dep);
+      if (fun_ty.tag === "Hol") {
+        var ctx = fun_ty.ctx;
+        var got = fun_ty.got;
+        fun_ty = All(
+          Hol(num_to_str(FRESH++), ctx, got), x =>
+          Hol(num_to_str(FRESH++), Cons([num_to_str(dep),x], ctx), got));
+      }
       if (fun_ty.tag === "All") {
         check(load, val.arg, fun_ty.inp, true, dep);
         return fun_ty.bod(val.arg);
       } else {
+        console.log("- " + show_term(val.fun, dep));
+        console.log("- " + show_term(fun_ty, dep));
         throw "NonFunApp";
+      }
+    }
+    case "Ins": {
+      var val_ty = reduce(load, infer(load, val.val, dep), dep);
+      if (val_ty.tag === "Slf") {
+        return val_ty.bod(val);
+      } else {
+        throw "NonSlfIns";
       }
     }
     case "Slf": {
@@ -187,9 +218,9 @@ export function infer(load: Load, val: Term, dep: number = 0): Term {
 // Checks if a type of a term is correct.
 export function check(load: Load, val: Term, tty: Term, chk: boolean, dep: number = 0): Term {
   //console.log("chk", show_term(val, dep));
-  //console.log(" ::", show_term(typ, dep));
+  //console.log(" ::", show_term(tty, dep));
   var typ = reduce(load, tty, dep);
-  var typ = typ.tag === "Slf" ? typ.bod(val) : typ;
+  //var typ = typ.tag === "Slf" ? typ.bod(val) : typ;
   if (!chk) {
     return typ;
   } else {
@@ -202,9 +233,17 @@ export function check(load: Load, val: Term, tty: Term, chk: boolean, dep: numbe
         //console.log("- ", show_term(typ, dep));
         throw "NonFunLam";
       }
+    } else if (val.tag === "Ins") {
+      if (typ.tag === "Slf") {
+        check(load, val.val, typ.bod(val), chk, dep);
+        return typ;
+      } else {
+        throw "NonSlfIns";
+      }
     } else if (val.tag === "Hol") {
+      console.log("HOLE: ?" + val.nam);
       console.log("GOAL: " + show_term(normal(x => null, tty, dep), dep));
-      console.log("HOLE: ?" + val.nam + "\n" + fold(val.ctx, ([nam,val],str) => str + "- " + nam + " = " + show_term(val, dep) + "\n", "").trim());
+      console.log(fold(val.ctx, ([nam,val],str) => str + "- " + nam + " = " + show_term(val, dep) + "\n", "").trim());
       return typ;
     } else if (val.tag === "Let") {
       var val_typ = infer(load, val.val, dep);
@@ -216,7 +255,7 @@ export function check(load: Load, val: Term, tty: Term, chk: boolean, dep: numbe
     } else {
       var inf = infer(load, val, dep);
       var inf = reduce(load, inf, dep);
-      var inf = inf.tag === "Slf" ? inf.bod(val) : inf;
+      //var inf = inf.tag === "Slf" ? inf.bod(val) : inf;
       if (!equal(load, typ, inf, dep)) {
         var exp = show_term(typ, dep);
         var det = show_term(inf, dep);
@@ -265,16 +304,19 @@ export function cut(str: string): string {
 export function show_term(term: Term, dep: number = 0): string {
   switch (term.tag) {
     case "Var": return num_to_str(term.val);
+    //case "Var": return "@" + term.val;
     case "Set": return "*";
-    case "All": return "∀(" + num_to_str(dep) + ":"+show_term(term.inp, dep)+")" + show_term(term.bod(Var(dep)), dep + 1);
+    case "All": return "∀(" + num_to_str(dep) + ":"+show_term(term.inp, dep)+") " + show_term(term.bod(Var(dep)), dep + 1);
     case "Lam": return "λ" + num_to_str(dep) + " " + show_term(term.bod(Var(dep)), dep + 1);
     case "App": return "(" + cut(show_term(term.fun, dep)) + " " + show_term(term.arg, dep) + ")";
     case "Slf": return "$" + num_to_str(dep) + " " + show_term(term.bod(Var(dep)), dep+1);
+    case "Ins": return "~" + show_term(term.val, dep);
     case "Ann": return "{" + show_term(term.val, dep) + ":" + show_term(term.typ, dep) + "}";
     case "Let": return "let " + num_to_str(dep) + " = " + show_term(term.val, dep) + " " + show_term(term.bod(Var(dep)), dep + 1);
     case "Def": return "def " + num_to_str(dep) + " = " + show_term(term.val, dep) + " " + show_term(term.bod(Var(dep)), dep + 1);
     case "Ref": return term.nam;
-    case "Hol": return "?" + term.nam + "{" + fold(term.ctx, ([nam,val],str) => str + nam + "=" + show_term(val, dep) + ";", "") + "}";
+    //case "Hol": return "?" + term.nam + "{" + fold(term.ctx, ([nam,val],str) => str + nam + "=" + show_term(val, dep) + ";", "") + "}";
+    case "Hol": return "(?" + term.nam + fold(term.got, (val,str) => str + " " + show_term(val, dep), "") + ")";
   }
 }
 
@@ -300,6 +342,13 @@ export function find<A>(list: Context, nam: string): Term {
   switch (list.tag) {
     case "Cons": return list.head[0] === nam ? list.head[1] : find(list.tail, nam);
     case "Nil" : return Ref(nam);
+  }
+}
+
+export function gets<A>(list: Context, idx: number): Term {
+  switch (list.tag) {
+    case "Cons": return idx === 0 ? list.head[1] : gets(list.tail, idx - 1);
+    case "Nil" : throw "unbound";
   }
 }
 
@@ -382,6 +431,11 @@ export function parse_term(code: string): [string, (ctx: Context) => Term] {
     var [code, bod] = parse_term(code);
     return [code, ctx => Slf(x => bod(Cons([nam, x], ctx)))];
   }
+  // INS: `~t`
+  if (code[0] === "~") {
+    var [code, val] = parse_term(code.slice(1));
+    return [code, ctx => Ins(val(ctx))];
+  }
   // ANN: `{x : T}`
   if (code[0] === "{") {
     var [code, val] = parse_term(code.slice(1));
@@ -409,7 +463,12 @@ export function parse_term(code: string): [string, (ctx: Context) => Term] {
   // HOL: `?` `name`
   if (code[0] === "?") {
     var [code, nam] = parse_name(code.slice(1));
-    return [code, ctx => Hol(nam, ctx)];
+    return [code, ctx => Hol(nam, ctx, Nil())];
+  }
+  // VAR: `%idx`
+  if (code[0] === "%") {
+    var [code, idx] = parse_name(code.slice(1));
+    return [code, ctx => gets(ctx, Number(idx))];
   }
   // VAR: `name`
   var [code, nam] = parse_name(code);
@@ -445,3 +504,26 @@ export function parse_book(code: string): Book {
 export function do_parse_book(code: string): Book {
   return parse_book(code);
 }
+
+//var code = `
+//Bool
+//: *
+//= $self
+  //∀(P: ∀(x: Bool) *) 
+  //∀(t: (P true))
+  //∀(f: (P false))
+  //(P self)
+
+//true
+//: Bool
+//= ~ λP λt λf t
+
+//false
+//: Bool
+//= ~ λP λt λf f
+//`;
+
+//var book = parse_book(code);
+
+//check_all(book);
+
